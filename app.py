04d061ae69770f9b51ed7b7cf94c4cd7f2309abc
ENES TEKEŞ <enestekes01@gmail.com>
2025-05-16 10:27:21 +0300

    app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ grm3 = st.number_input("Grammar 3", min_value=0,max_value=100)
 st.subheader("Writing Quizzes")
 wrt1 = st.number_input("Writing 1", min_value=0,max_value=100)
 wrt2 = st.number_input("Writing 2", min_value=0,max_value=100)
-wrt3 = st.number_input("Writing 3", min_value=0,max_value=100)
+
 
 st.subheader("Other Quizzes")
 dbt = st.number_input("Debate", min_value=0,max_value=100)
@@ -20,7 +20,7 @@ mid = st.number_input("Midterm", min_value=0,max_value=100)
 final = st.number_input("EMT", min_value=0,max_value=100, help="If the grade is not available yet, you can leave it blank.")
 
 if st.button("Calculate"):
-    result = (((grm1 + grm2 + grm3)/3)*0.1) + (((wrt1 + wrt2 + wrt3)/3)*0.1) + (dbt*0.05) + (prj*0.1) + (ohmw*0.05) + (mid*0.2)
+    result = (((grm1 + grm2 + grm3)/3)*0.1) + (((wrt1 + wrt2)/2)*0.1) + (dbt*0.1) + (prj*0.05) + (ohmw*0.05) + (mid*0.2)
     resultforfinal = ((60-result)*2.5)
     st.success(f"Your Score: {round(result, 2)}")
     st.success(f"Required Marks(Only for EMT): {round(resultforfinal,3)}")
