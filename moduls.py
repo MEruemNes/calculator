cf41299d5cb449bc2783024a9b2f5bdf1538f699
ENES TEKEŞ <enestekes01@gmail.com>
2025-05-16 10:54:44 +0300

    Add files via upload

---
+++ moduls.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+
+modul = input("Modülünüzü giriniz(1,2,3,4): ")
+
+if modul == "1":
+     webbrowser.open("https://calculator-jctqjn5zjrrasjtf2jw5wi.streamlit.app/")
+elif modul == "2":
+     webbrowser.open("https://calculator-jctqjn5zjrrasjtf2jw5wi.streamlit.app/")
+elif modul == "3":
+     webbrowser.open("https://calculator-jctqjn5zjrrasjtf2jw5wi.streamlit.app/")
+else:
+     webbrowser.open("https://calculator-jctqjn5zjrrasjtf2jw5wi.streamlit.app/")
