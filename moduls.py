5dd94eb01c82dbf83a58bfb6c44616cb5e44f4e3
ENES TEKEŞ <enestekes01@gmail.com>
2025-05-20 12:26:03 +0300

    moduls.py

--- moduls.py
+++ moduls.py
@@ -4,10 +4,10 @@ import webbrowser
 modul = input("Modülünüzü giriniz(1,2,3,4): ")
 
 if modul == "1":
-     webbrowser.open("https://calculator-jctqjn5zjrrasjtf2jw5wi.streamlit.app/")
+     webbrowser.open("https://calculator-zgkkmd8uwzx7opoph6p9af.streamlit.app/")
 elif modul == "2":
-     webbrowser.open("https://calculator-jctqjn5zjrrasjtf2jw5wi.streamlit.app/")
+     webbrowser.open("https://modals-3h6eb69pkbcyk63hgxux98.streamlit.app/")
 elif modul == "3":
-     webbrowser.open("https://calculator-jctqjn5zjrrasjtf2jw5wi.streamlit.app/")
+     webbrowser.open("https://calculator-spsxvhcsbgr9fj4v9f8mmg.streamlit.app/")
 else:
      webbrowser.open("https://calculator-jctqjn5zjrrasjtf2jw5wi.streamlit.app/")
