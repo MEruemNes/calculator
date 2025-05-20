74b378b1163dc3d37759a7d9e9b4d039f3d76a8a
ENES TEKEŞ <enestekes01@gmail.com>
2025-05-20 12:48:14 +0300

    moduls.py

--- moduls.py
+++ moduls.py
@@ -1,13 +1,25 @@
-import webbrowser
+import streamlit as st
 
+st.set_page_config(page_title="Modül Seçimi", layout="centered")
 
-modul = input("Modülünüzü giriniz(1,2,3,4): ")
+st.title("📚 Modül Seçimi Yapınız")
 
-if modul == "1":
-     webbrowser.open("https://calculator-zgkkmd8uwzx7opoph6p9af.streamlit.app/")
-elif modul == "2":
-     webbrowser.open("https://modals-3h6eb69pkbcyk63hgxux98.streamlit.app/")
-elif modul == "3":
-     webbrowser.open("https://calculator-spsxvhcsbgr9fj4v9f8mmg.streamlit.app/")
-else:
-     webbrowser.open("https://calculator-jctqjn5zjrrasjtf2jw5wi.streamlit.app/")
+modul = st.radio(
+    "Lütfen bir modül seçin:",
+    ("1 - A1", "2 - A2", "3 - B1", "4 - B2")
+)
+
+linkler = {
+    "1 - A1": "https://calculator-zgkkmd8uwzx7opoph6p9af.streamlit.app/",
+    "2 - A2": "https://modals-3h6eb69pkbcyk63hgxux98.streamlit.app/",
+    "3 - B1": "https://calculator-spsxvhcsbgr9fj4v9f8mmg.streamlit.app/",
+    "4 - B2": "https://calculator-jctqjn5zjrrasjtf2jw5wi.streamlit.app/"
+}
+
+if st.button("Devam Et"):
+    secilen_link = linkler.get(modul)
+    js = f"""
+    <meta http-equiv="refresh" content="0; url={secilen_link}">
+    <p>Yönlendiriliyorsunuz... Eğer yönlendirilmezseniz <a href="{secilen_link}">buraya tıklayın</a>.</p>
+    """
+    st.markdown(js, unsafe_allow_html=True)
